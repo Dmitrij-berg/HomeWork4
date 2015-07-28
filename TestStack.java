486ba298b426ac6e6acdd313ff94a87fdb0a14f3
Dmitrij Beregovoj <dmitrij.berg@gmail.com>
2015-07-28 18:00:40 +0300

    HomeWork4 (исправленно)

--- TestStack.java
+++ TestStack.java
@@ -1,42 +1,85 @@
 package HomeWork4;
 
+
 public class TestStack {
 
+    Stack mystack1 = new StackImpl(5);
+
+    // Добавление четырех элементов
+    public  void addfour() throws StackException {
+        for(int i = 1; i < 5; ++i) {
+            mystack1.push("E" + i);
+        }
+    }
+
+    // Удаление двух элементов
+    public  void delltwo() throws StackException {
+        for(int i = 1; i < 5; i++) mystack1.pop();
+        for(int i = 1; i < 6; i++){
+            switch (i){
+                case 1: mystack1.push(null);break;
+                case 2: mystack1.push(null); break;
+                case 3: mystack1.push("E3"); break;
+                case 4: mystack1.push("E4"); break;
+                case 5: mystack1.push(null); break;
+                default: break;
+            }
+        }
+    }
+
+    // Добавление трех элементов
+    public  void addthree() throws StackException{
+        for(int i = 1; i < 6; i++) mystack1.pop();
+        for(int i = 1; i < 6; i++){
+            switch (i){
+                case 1: mystack1.push("E6"); break;
+                case 2: mystack1.push("E7"); break;
+                case 3: mystack1.push("E3"); break;
+                case 4: mystack1.push("E4"); break;
+                case 5: mystack1.push("E5"); break;
+                default: break;
+            }
+        }
+    }
+
+
     public static void main(String[] args) throws StackException {
-        StackImpl mystack1 = new StackImpl(5);
+
+        TestStack St = new TestStack();
         try {
 
-            mystack1.print();   // Вывод всех элементов стека
+            St.mystack1.print();   // Вывод всех элементов стека
 
-            mystack1.addfour(); // Добавление четырех элементов
+            St.addfour(); // Добавление четырех элементов
 
-            mystack1.print();   // Вывод всех элементов стека
+            St.mystack1.print();   // Вывод всех элементов стека
 
-            mystack1.delltwo(); // Удаление двух элементов
+            St.delltwo(); // Удаление двух элементов
 
-            mystack1.print();   // Вывод всех элементов стека
+            St.mystack1.print();   // Вывод всех элементов стека
 
-            mystack1.addthree(); // Добавление трех элементов
+            St.addthree(); // Добавление трех элементов
 
-            mystack1.print();    // Вывод всех элементов стека
+            St.mystack1.print();    // Вывод всех элементов стека
 
-         //   mystack1.push("E8");  // Попытка добавить элемент в полный стек
+            // St.mystack1.push("E8");  // Попытка добавить элемент в полный стек
 
-            mystack1.clear();       // Очистка стека
+            St.mystack1.clear();       // Очистка стека
 
-            mystack1.print();    // Вывод всех элементов стека
+            St.mystack1.print();    // Вывод всех элементов стека
 
-         // mystack1.pop(); // Попытка удалить элемент из стека
+            // St.mystack1.pop(); // Попытка удалить элемент из стека
 
         } catch (StackException e){
             System.out.println(e);
         }
     }
+
 }
 
 
 
- interface Stack {
+interface Stack {
     // adds an element to stack, throws exception if stack is full ("StackImpl is
     //full" message)
     void push(Object o) throws StackException;
@@ -46,21 +89,23 @@ public class TestStack {
     void clear();
     // prints all elements of stack to standart output
     void print();
+
 }
 
 
 
 class StackImpl implements Stack {
-   private Object stck[];
-   private int tos;
+    private Object stck[];
+    private int tos;
 
     StackImpl(int size){
         stck = new  Object[size] ;
         tos = -1;
     }
 
+
     //  Добавление элемента
-    public void push(Object o) throws StackException {
+    public  void push(Object o) throws StackException {
         if(tos ==  stck.length-1){
             throw new StackException( "Stack is full");
         }else { stck[++tos] = o;}
@@ -89,46 +134,6 @@ class StackImpl implements Stack {
         }
         System.out.println(" ");
     }
-
-
-    // Добавление четырех элементов
-    public  void addfour() throws StackException {
-        for(int i = 1; i < 5; ++i){
-            push("E" + i);
-        }
-    }
-
-    // Удаление двух элементов
-    public void delltwo() throws StackException {
-        for(int i = 1; i < 5; i++) pop();
-        for(int i = 1; i < 6; i++){
-            switch (i){
-                case 1: push(null); break;
-                case 2: push(null); break;
-                case 3: push("E3"); break;
-                case 4: push("E4"); break;
-                case 5: push(null); break;
-                default: break;
-            }
-        }
-    }
-
-
-    // Добавление трех элементов
-    public  void addthree() throws StackException{
-        for(int i = 1; i < 6; i++) pop();
-        for(int i = 1; i < 6; i++){
-            switch (i){
-                case 1: push("E6"); break;
-                case 2: push("E7"); break;
-                case 3: push("E3"); break;
-                case 4: push("E4"); break;
-                case 5: push("E5"); break;
-                default: break;
-            }
-        }
-    }
-
-
-
 }
+
+
